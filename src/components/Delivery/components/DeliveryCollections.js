d2d6e79ede4f5d8354350d28c4b1fb4220d31fcb
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-12 17:38:57 +0545

    added setup for delivery collections component

---
+++ src/components/Delivery/components/DeliveryCollections.js
@@ -0,0 +1,11 @@
+const DeliveryCollections = () => {
+  return (
+    <div className="delivery-collection ">
+      <div className="max-width">
+        <p className="collection-title">Eat what makes you happy</p>
+      </div>
+    </div>
+  );
+};
+
+export default DeliveryCollections;
--- src/components/Delivery/index.js
+++ src/components/Delivery/index.js
@@ -1,14 +1,17 @@
 // Components
 import Filter from '../common/Filter';
+import DeliveryCollections from './components/DeliveryCollections';
 
 // Constants
 import { deliveryFilters } from '../../constants';
 
 const Delivery = () => {
   return (
-    <div className="max-width">
-      <Filter filterList={deliveryFilters} />
-      This is Delivery
+    <div>
+      <div className="max-width">
+        <Filter filterList={deliveryFilters} />
+        <DeliveryCollections />
+      </div>
     </div>
   );
 };
