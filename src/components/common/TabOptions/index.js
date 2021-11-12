83a985610b4f0731fec42164b40a5ededf22f74a
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-12 15:35:31 +0545

    added basic components for homepage

---
+++ src/components/common/TabOptions/index.js
@@ -0,0 +1,5 @@
+const TabOptions = () => {
+  return <div>This is the TabOptions</div>;
+};
+
+export default TabOptions;
--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+
+import Header from '../../components/common/Header';
+import Footer from '../../components/common/Footer';
+import TabOptions from '../../components/common/TabOptions';
+
 const HomePage = () => {
-  return <div>Homepage</div>;
+  const [activeTab, setActiveTab] = useState('Delivery');
+
+  const getCorrectScreen = (tab) => {
+    switch (tab) {
+      case 'Delivery':
+        return <div>Delivery</div>;
+      case 'Dining Out':
+        return <div>Dining Out</div>;
+      case 'Night Life':
+        return <div>Night Life</div>;
+      default:
+        return <div>Delivery</div>;
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <TabOptions />
+      {getCorrectScreen(activeTab)}
+      <Footer />
+    </>
+  );
 };
 
 export default HomePage;
