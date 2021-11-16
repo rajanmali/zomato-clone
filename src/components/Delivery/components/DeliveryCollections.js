fbd72206e46e1eab0a6515e156779047c2ba5cee
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-16 10:28:33 +0545

    file setup for carousel component

--- src/components/Delivery/components/DeliveryCollections.js
+++ src/components/Delivery/components/DeliveryCollections.js
@@ -1,3 +1,13 @@
+import { deliveryItems } from '../../../constants/deliveryItems';
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
 const DeliveryCollections = () => {
   return (
     <div className="delivery-collection ">
---
+++ src/components/common/Carousel/components/PrevArrow/index.js
@@ -0,0 +1,5 @@
+const PrevArrow = () => {
+  return <div></div>;
+};
+
+export default PrevArrow;
