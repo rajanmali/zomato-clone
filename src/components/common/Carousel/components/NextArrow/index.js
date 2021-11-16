b66bff48844f1f7aa7a154f8919fc4e2eedc0cf8
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-16 10:31:25 +0545

    added styling for next arrow

--- src/components/common/Carousel/components/NextArrow/index.js
+++ src/components/common/Carousel/components/NextArrow/index.js
@@ -1,5 +1,17 @@
-const LeftArrow = () => {
-  return <div></div>;
-};
+const NextArrow = ({ className, style, onClick }) => (
+  <div
+    className={className}
+    style={{
+      ...style,
+      background: 'white',
+      borderRadius: '50%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: '4px',
+    }}
+    onClick={onClick}
+  />
+);
 
-export default LeftArrow;
+export default NextArrow;
